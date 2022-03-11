d2c4a2c27c476fd988a5758831a3f1a1093eefd1
yerin512 <sosu0512@naver.com>
2022-03-11 18:31:25 +0900

    4_type-aliases

---
+++ class-note/4_type-aliases.ts
@@ -0,0 +1,22 @@
+// interface Person {
+//     name: string;
+//     age: number;
+// }
+
+type Person2 = {
+    name: string,
+    age: number,
+}
+
+var yerin2: Person2 = {
+    name: '예린',
+    age: 33
+}
+
+type MyString = string;
+var str: MyString = 'hello';
+
+type Todo = { id: string; title: string; done: boolean };
+function getTodo(todo: Todo) {
+    
+}
